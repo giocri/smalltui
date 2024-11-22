8bde32d94f05e9b69f60c6bab7c64ce906ea1f84
giocri <67639762+giocri@users.noreply.github.com>
2024-11-22 15:58:05 +0100

    extended widget to work on boxed widgets

--- src/renderer/widget.rs
+++ src/renderer/widget.rs
@@ -3,3 +3,9 @@ use super::painter::Painter;
 pub trait Widget<P: Painter>: Sync + Send {
     fn render_widget(&self, painter: &mut P);
 }
+
+impl<P: Painter, T: Widget<P>> Widget<P> for Box<T> {
+    fn render_widget(&self, painter: &mut P) {
+        self.as_ref().render_widget(painter);
+    }
+}
